8f7575f3f170c48c9cc76c20ec26aa1bdee5d3f1
Minh Nguyen <minhhainguyen@kms-technology.com>
2019-08-16 20:19:02 +0700

    FLO-69 add route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Calculation from "./components/Calculation";
 
-function App() {
-  return <div className="App">hello</div>;
+function Index() {
+  return <h2>Home</h2>;
 }
 
-export default App;
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/calculation/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/calculation/" component={Calculation} />
+        <Route path="/users/" component={Users} />
+      </div>
+    </Router>
+  );
+}
+
+export default AppRouter;
