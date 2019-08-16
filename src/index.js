15b7421376e1c6591e367a9dd9b8fedc9bec77bf
euginsane <hoangtrinh@kms-technology.com>
2019-08-16 21:48:15 +0700

    update new packages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "antd/dist/antd.css";
+import axios from "axios";
 import * as serviceWorker from "./serviceWorker";
+
+axios.defaults.baseURL = "http://192.168.33.27:3000";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
