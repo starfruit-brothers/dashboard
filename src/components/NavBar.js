268e9a0490776bdabfadd86b19c8b81d2cd08d5e
Minh Nguyen <minhhainguyen@kms-technology.com>
2019-08-17 12:41:06 +0700

    fix navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,16 +11,16 @@ function NavBar() {
       <Menu
         theme="dark"
         mode="horizontal"
-        defaultSelectedKeys={["1"]}
+        defaultSelectedKeys={[window.location.pathname]}
         style={{ lineHeight: "64px" }}
       >
-        <Menu.Item key="1">
+        <Menu.Item key="/">
           <Link to="/">Home</Link>
         </Menu.Item>
-        <Menu.Item key="2">
+        <Menu.Item key="/calculation">
           <Link to="/calculation">Calculation</Link>
         </Menu.Item>
-        <Menu.Item key="3">
+        <Menu.Item key="/users">
           <Link to="/users">Users</Link>
         </Menu.Item>
       </Menu>
